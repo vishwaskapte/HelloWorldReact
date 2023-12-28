4c171d6f27e0fe6ac67b8d9d83e85ac621bd2efe
vishwaskapte <vishwaskapte@gmail.com>
2023-12-28 13:39:43 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Comp1 from './Comp1';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Comp1 />
     </div>
   );
 }
